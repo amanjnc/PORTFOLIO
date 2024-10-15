f52e658c2aee6cd862c7fafd65fddfdf3243eb94
amanuel <amanuelbeyne662@gmail.com>
2024-10-15 14:21:13 +0300

    figma added

--- src/constants/socialMedia.js
+++ src/constants/socialMedia.js
@@ -1,6 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTelegram } from "@fortawesome/free-brands-svg-icons";
-import { AiFillGithub, AiFillLinkedin, AiFillMail } from "react-icons/all";
+import {
+	AiFillGithub,
+	AiFillLinkedin,
+	AiFillMail,
+	SiFigma,
+} from "react-icons/all";
 
 export const SOCIAL_MEDIA = [
 	{
@@ -23,5 +28,11 @@ export const SOCIAL_MEDIA = [
 		id: "social-media-4",
 		icon: faTelegram,
 		link: "https://t.me/Portal_0",
+	},
+
+	{
+		id: "social-media-5",
+		icon: SiFigma,
+		link: "https://www.figma.com/@amanuel_beyene",
 	},
 ];
